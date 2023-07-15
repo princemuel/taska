ae6de5fc305d827a03a97d14ed9d993eac1aa054
Prince Muel <vansomecsam@gmail.com>
2023-07-15 04:02:16 +0100

    feat: add helper functions
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

---
+++ lib/helpers/helpers.ts
@@ -0,0 +1,174 @@
+import { cx } from "cva";
+import { ClassValue } from "cva/dist/types";
+import { AssertionError } from "node:assert";
+import { extendTailwindMerge } from "tailwind-merge";
+
+const customTwMerge = extendTailwindMerge({
+  // classGroups: {
+  //   'font-size': [{ text: ['100', '200', '300', '400', '500', '600', '700'] }],
+  // },
+});
+
+export function cn(...args: ClassValue[]) {
+  return customTwMerge(cx(args));
+}
+
+/*---------------------------------*
+            STRING UTILS           *
+  ---------------------------------*
+ */
+
+export function capitalize(string = "") {
+  return string[0].toUpperCase() + string.slice(1).toLowerCase();
+}
+export const trim = (string = "") => string.trim();
+
+export function removeFirstChar(string = "") {
+  return string?.slice(1);
+}
+export function pluralize(word: string, value: number) {
+  return value === 1 ? `${word}` : `${word}s`;
+}
+
+export function truncate(word = "", length = word.length) {
+  return word.length > length ? `${word.substring(0, length)}...` : word;
+}
+
+type EndsWith<W, S extends string> = W extends `${infer _R}${S}` ? W : never;
+
+export const endsWith = <Word extends string, Suffix extends string>(
+  str: Word,
+  suffix: Suffix
+): str is EndsWith<Word, Suffix> => {
+  return str.endsWith(suffix);
+};
+
+/*---------------------------------*
+            OBJECT UTILS           *
+  ---------------------------------*
+ */
+
+export function serialize<T>(data: T): NonNullable<T> {
+  return JSON.parse(JSON.stringify(data));
+}
+
+export const objectKeys = <O extends {}>(object: O): (keyof O)[] => {
+  return Object.keys(object) as (keyof O)[];
+};
+
+/*---------------------------------*
+            ARRAY UTILS           *
+  ---------------------------------*
+ */
+
+export function hasValues<T>(
+  array: T[] | null | undefined
+): array is NonNullable<T[]> {
+  return (array || []).length > 0;
+}
+
+/**
+ * A Generic Ranking Algorithm
+ * @param items T[]
+ * @param order 'asc' | 'desc'
+ * @returns An array containing the sorted items according to the ranking algorithm
+ */
+
+export const rank = <T>(
+  items: T[],
+  order: "asc" | "desc",
+  callbackfn: (value: T) => number
+) => {
+  return items
+    .map((item) => ({
+      item,
+      rank: callbackfn(item),
+    }))
+    .sort((a, b) => (order === "asc" ? a.rank - b.rank : b.rank - a.rank))
+    .map((ranked) => ranked.item);
+};
+
+/*---------------------------------*
+            FUNCTION UTILS         *
+  ---------------------------------*
+ */
+
+export function assert(condition: any, message?: string): asserts condition {
+  if (!condition) {
+    throw new AssertionError({ message });
+  }
+}
+
+/*---------------------------------*
+            DATE UTILS             *
+  ---------------------------------*
+ */
+
+type FormatDateFunction = (
+  date?: string,
+  formatOptions?: Intl.DateTimeFormatOptions[],
+  separator?: string
+) => string;
+
+export const formatDate: FormatDateFunction = (
+  date = new Date().toISOString(),
+  formatOptions = [{ day: "numeric" }, { month: "short" }, { year: "numeric" }],
+  separator = " "
+) => {
+  return formatOptions
+    .map((options) => {
+      const dateFormatter = new Intl.DateTimeFormat("en", options);
+      return dateFormatter.format(new Date(date));
+    })
+    .join(separator);
+};
+/*---------------------------------*
+            DOM UTILS              *
+  ---------------------------------*
+ */
+
+export const isBrowser = typeof window !== "undefined";
+export const isNavigator = typeof navigator !== "undefined";
+
+export function on<T extends Window | Document | HTMLElement | EventTarget>(
+  obj: T | null,
+  ...args: Parameters<T["addEventListener"]> | [string, Function | null, ...any]
+): void {
+  if (obj && obj.addEventListener) {
+    obj.addEventListener(
+      ...(args as Parameters<HTMLElement["addEventListener"]>)
+    );
+  }
+}
+
+export function off<T extends Window | Document | HTMLElement | EventTarget>(
+  obj: T | null,
+  ...args:
+    | Parameters<T["removeEventListener"]>
+    | [string, Function | null, ...any]
+): void {
+  if (obj && obj.removeEventListener) {
+    obj.removeEventListener(
+      ...(args as Parameters<HTMLElement["removeEventListener"]>)
+    );
+  }
+}
+
+export const shimmer = (width: number, height: number) => `
+<svg width="${width}" height="${height}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <defs>
+    <linearGradient id="g">
+      <stop stop-color="#333" offset="20%" />
+      <stop stop-color="#222" offset="50%" />
+      <stop stop-color="#333" offset="70%" />
+    </linearGradient>
+  </defs>
+  <rect width="${width}" height="${height}" fill="#333" />
+  <rect id="r" width="${width}" height="${height}" fill="url(#g)" />
+  <animate xlink:href="#r" attributeName="x" from="-${width}" to="${width}" dur="1s" repeatCount="indefinite"  />
+</svg>`;
+
+export const toBase64 = (str: string) =>
+  typeof window === "undefined"
+    ? Buffer.from(str).toString("base64")
+    : window?.btoa(str);
---
+++ lib/helpers/index.ts
@@ -0,0 +1,3 @@
+export * from "./cache";
+export * from "./fetcher";
+export * from "./helpers";
